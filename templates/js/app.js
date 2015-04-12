b0cf34688dad828ac8ad28edda5ee8ec65a9af75
benpptung <benpptung@tacol.biz>
2015-04-12 22:32:48 +0800

    add swig and incorporate appstackr, drop jade, because appstackr can not support jade

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,26 +1,42 @@
-var express = require('express');
 var path = require('path');
+
+var express = require('express');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dhs = require('domain-http-server');{swig.require}
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// app setup
+app.disable('etag');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '{views}');
 
+if (app.get('env') != 'production') {
+  app.set('view cache', false);{swig.cache}
+}
+
+app.use(dhs);
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());{css}
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.set({
+    "Cache-Control": "private, no-cache, no-store, must-revalidate",
+    "Expires": "Thu, 15 Apr 2000 20:00:00 GMT",
+    "Pragma" : "no-cache"
+  });
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -33,26 +49,14 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
+    code: res.statusCode,
     message: err.message,
-    error: {}
+    error: app.get('env') == 'production' ? {} : err
   });
 });
 
---
+++ templates/js/stacks.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = [
+  {
+    name: 'base/site',
+    nature: 'css',
+    files: [
+      'client/site/style.{css}'
+    ]
+  },
+  {
+    name: 'base/site',
+    nature: 'js',
+    files: [
+      'client/site/env.js',
+      'superagent',
+      'node_modules/ractive/ractive-legacy.runtime.min.js',
+      'ractive-events-tap/ractive-events-tap.min.js'
+    ],
+    browserify: {
+      exposes: 'ractive-legacy.runtime.min.js:ractive, superagent'
+    }
+  },
+  {
+    name: 'base/iefix',
+    nature: 'js',
+    files: [
+      'node_modules/html5shiv/dist/html5shiv.min.js',
+      'node_modules/respond.js/dest/respond.matchmedia.addListener.min.js'
+    ]
+  }
+];
