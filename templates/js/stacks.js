d57289dbdcacf795c0c8d2d69786223315a0190a
benpptung <benpptung@tacol.biz>
2016-02-11 18:31:36 +0800

    add react-dom and react-addons modules

--- templates/js/stacks.js
+++ templates/js/stacks.js
@@ -11,16 +11,31 @@ module.exports = [
   {
     name: 'base/site',
     nature: 'js',
-    files: 'node_modules/react/dist/react-with-addons.js',
+    files: [
+      'node_modules/react/dist/react-with-addons.js',
+      'react-dom'
+    ],
     browserify: {
-      exposes: 'react-with-addons.js:react'
+      exposes: 'react-with-addons.js:react, react-dom'
     }
   },
   {
     name: 'base/bundle',
     nature: 'js',
     files: [
-      'superagent'
+      'superagent',
+
+      // following React Add-Ons do not require react/lib/xxx,
+      // so no more multiple React copies loaded error.
+      // Remove the followings, if they are extraneous
+      'react-addons-css-transition-group',
+      'react-addons-update',
+      'react-addons-transition-group',
+      'react-addons-shallow-compare',
+      'react-addons-pure-render-mixin',
+      'react-addons-linked-state-mixin',
+      'react-addons-clone-with-props',
+      'react-addons-create-fragment'
     ],
     browserify: {
       exposes: '*', externals: 'react'
